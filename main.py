39be48517e04b52a979ee48040aa93dd0a6904e1
artem <a.divencko@gmail.com>
2023-01-17 07:14:46 -0600

    added command line interface

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ import pytesseract
 import cv2
 import os
 import pandas as pd
+import argparse
+import sys
 pd.options.mode.chained_assignment = None
 
 
+parser = argparse.ArgumentParser(
+    prog = 'image-classification',
+    description = 'Summarize reciept credentials'
+)
+
+parser.add_argument('--file', help="absolute path to file or folder with reciept image (*.jpg)", required=True)
+
+args = parser.parse_args()
+
+if args.file is None:
+    print('Error, cannot read input file: No such file or directory'); sys.exit()
+
+print(args.file)
 # get the path/directory
-im_box = ['X00016469612', 'X00016469622', 'X00016469672']
-folder_dir = "../train"; box_dir = "../train/box"; vis_box = "../train/vis_box"
+im_box = ['X00016469623', 'X00016469669'] # segmenation examples
+folder_dir = args.file; box_dir = args.file + '/box'; vis_box = args.file + '/vis_box'
 try:
     os.makedirs(box_dir); os.makedirs(vis_box)
 except OSError:
@@ -37,7 +52,6 @@ for im in os.listdir(folder_dir):
             f.write(f'{x1[i]},{y1[i]},{x2[i]},{y1[i]},{x2[i]},{y2[i]},{x1[i]},{y2[i]}, {lines[i]}\n')
         f.close()
         if file_name in im_box:
-            print(vis_box + '/' + file_name + '.jpg')
             for i in range(len(lines)):
                 cv2.rectangle(img, (x1[i],y1[i]), (x2[i],y2[i]) , (0,255,0), 2)
             cv2.imwrite(vis_box + '/' + file_name + '.jpg', img)
