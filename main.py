af7c739175924a57204c14c99a13ec894172668e
artem <a.divencko@gmail.com>
2023-01-17 05:16:18 -0600

    some fixes

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ pd.options.mode.chained_assignment = None
 
 
 # get the path/directory
-folder_dir = "./testing"
+im_box = ['X00016469612', 'X00016469622', 'X00016469672']
+folder_dir = "../train"; box_dir = "../train/box"; vis_box = "../train/vis_box"
+try:
+    os.makedirs(box_dir); os.makedirs(vis_box)
+except OSError:
+    pass
 for im in os.listdir(folder_dir):
     if im.endswith(".jpg"):
-        f = open(im[:im.find('.')] + ".txt", mode = "w")
+        file_name = im[:im.find('.')]
+        f = open(os.path.join(box_dir, file_name + ".txt"), mode = "w")
         img = cv2.imread(folder_dir + '/' + im)
 
         d = pytesseract.image_to_data(img, output_type='data.frame')
@@ -30,14 +36,8 @@ for im in os.listdir(folder_dir):
         for i in range(len(lines)):  
             f.write(f'{x1[i]},{y1[i]},{x2[i]},{y1[i]},{x2[i]},{y2[i]},{x1[i]},{y2[i]}, {lines[i]}\n')
         f.close()
-
-
-# get image examples
-'''
-for i in range(len(line)):
-    text,x,y,w,h = d['text'][i],d['left'][i],d['top'][i],d['width'][i],d['height'][i]
-    cv2.rectangle(img, (x1[i],y1[i]), (x2[i],y2[i]) , (0,255,0), 2)
-
-cv2.imshow('img',img)
-cv2.waitKey(0)
-'''
+        if file_name in im_box:
+            print(vis_box + '/' + file_name + '.jpg')
+            for i in range(len(lines)):
+                cv2.rectangle(img, (x1[i],y1[i]), (x2[i],y2[i]) , (0,255,0), 2)
+            cv2.imwrite(vis_box + '/' + file_name + '.jpg', img)
